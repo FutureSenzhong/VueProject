4e25fcc6e17dee01ebda39ce345cc687a40d8180
dsz <1019217919@qq.com>
2021-02-05 20:32:04 +0800

    完成登录页面ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/account/Login";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   },
   {
     path: "/about",
@@ -19,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 });
 
--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,13 @@ module.exports = {
     //样式文件配置
     css: {
         loaderOptions: {
-            // 给 sass-loader 传递选项
-            sass: {
-                // @/ 是 src/ 的别名
-                // 所以这里假设你有 `src/variables.sass` 这个文件
-                // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
-                prependData: `@import "~@/variables.sass"`
-            },
+            // // 给 sass-loader 传递选项
+            // sass: {
+            //     // @/ 是 src/ 的别名
+            //     // 所以这里假设你有 `src/variables.sass` 这个文件
+            //     // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
+            //     prependData: `@import "~@/variables.sass"`
+            // },
             // 默认情况下 `sass` 选项会同时对 `sass` 和 `scss` 语法同时生效
             // 因为 `scss` 语法在内部也是由 sass-loader 处理的
             // 但是在配置 `prependData` 选项的时候
@@ -23,13 +23,13 @@ module.exports = {
                 prependData: `@import "./src/styles/main.scss";`
             },
             // 给 less-loader 传递 Less.js 相关选项
-            less: {
-                // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
-                // `primary` is global variables fields name
-                globalVars: {
-                    primary: '#fff'
-                }
-            }
+            // less: {
+            //     // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
+            //     // `primary` is global variables fields name
+            //     globalVars: {
+            //         primary: '#fff'
+            //     }
+            // }
         }
     }
 }
