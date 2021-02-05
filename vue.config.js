9350954f370bc53eb99cf4431aa4ca9428c9bdae
dsz <1019217919@qq.com>
2021-02-05 17:34:28 +0800

    语法检查开关

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     publicPath:process.env.NODE_ENV === "production" ? "" : "/",
+    // 打包路径
     outputDir:process.env.NODE_ENV === "production" ? "dist" : "devdist",
+    //语法检查开关
+    lintOnSave: true
 
 }
