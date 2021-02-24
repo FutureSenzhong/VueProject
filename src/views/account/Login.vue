f7cdc1a9342b021c3a2c7c6a8d21a9b2e4edb148
dsz <1019217919@qq.com>
2021-02-24 18:54:15 +0800

    登录页面输入空值验证

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -3,16 +3,19 @@
     <div class="form-wrap">
       <a-form
         name="custom-validation"
-        ref="ruleForm">
+        ref="ruleForm"
+        :model="account_form"
+        :rules="rules_form"
+        @finish="handleFinish">
 
-        <a-form-item>
+        <a-form-item name="username">
           <label>账号</label>
-          <a-input type="text" autocomplete="off" />
+          <a-input v-model:value="account_form.username" type="text" autocomplete="off" />
         </a-form-item>
 
-        <a-form-item>
+        <a-form-item name="password">
           <label>密码</label>
-          <a-input type="password" autocomplete="off" />
+          <a-input v-model:value="account_form.password" type="password" autocomplete="off" />
         </a-form-item>
 
         <a-form-item>
@@ -45,24 +48,63 @@ export default {
   name: "Login",
   components: { Captcha },
   setup(props) {
+    //自定义验证方法
+    const checkUsername = async (rule, value, callback) => {
+      if(!value){
+        return Promise.reject("账号必填")
+      } else {
+        return Promise.resolve()
+      }
+    };
+
+    const  checkPassword = async (rule, value, callback) => {
+      if(!value){
+        return Promise.reject("密码必填")
+      }else {
+        return Promise.resolve()
+      }
+    };
+
+    //定义表单数据
     const formConfig = reactive({
       layout: {
         labelCol: { span: 4 },
         wrapperCol: { span: 14 }
+      },
+      account_form: {
+        username: "",
+        password: "",
+      },
+      rules_form: {
+        username: [{validator: checkUsername, trigger: "change"}],
+        password: [{validator: checkPassword, trigger: "change"}],
       }
     });
     const data = toRefs(formConfig)
+
+    //提交表单的方法
+    const handleFinish = () => {
+      alert("登陆成功")
+    }
     //生命周期函数，挂摘完成之后
     onMounted(()=>{
 
     })
     return {
       // formConfig
-      ...data
+      ...data,
+      // 把方法返回才能调用
+      handleFinish
     };
   }
 };
 </script>
 <style lang="sass">
 @import "./src/styles/account/login.scss"
+</style>
+
+<style lang="scss">
+.ant-form-explain, .ant-form-extra {
+  text-align: left;
+}
 </style>
